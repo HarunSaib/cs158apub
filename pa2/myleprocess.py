cb7940329042e5e7d7293b0da8d417fd810e9c53
Harun Saib <144715661+HarunSaib@users.noreply.github.com>
2026-09-12 16:48:33 -0700

    config parse and comments

--- pa2/myleprocess.py
+++ pa2/myleprocess.py
@@ -4,7 +4,12 @@ import socket
 import threading
 import time
 
+process_uuid = uuid.uuid4()
+leader_id = None
+state = 0
 
+client_connection = None
+server_connection = None
 
 class Message:
     def __init__(self, uuid_val, flag=0):
@@ -14,7 +19,7 @@ class Message:
     # function to convert 'Message' class object into dict, to be converted into JSON and send
     def to_dict(self):
         return {
-            "uuid": str(self.uuid), # store uuid as str(ing)
+            "uuid": self.uuid, # store uuid as str(ing)
             "flag": self.flag # store flag as is
         } # pretty straight forward turning into dict
 
@@ -31,13 +36,27 @@ class Message:
 
 
 def read_config(filename="config.txt"):
+    with open(filename, "r") as file: # grab the file from the name passed in
+        # create list of lines in the file, strip()-ping whitespaces 
+        lines = [line.strip() for line in file if line.strip()] 
 
+    # small helper to unpack lines from the list
+    def parse_address(line):
+        ip, port = line.splot(",") # fetch ip and port seperated by comma
+        return ip.strip(), port.strip() # clean up whitespaces
+    
+    return parse_address(lines[0]), parse_address(lines[1])
 
-    pass
-
+# function to convert the processed uuid and flag dict into json and send to client
 def send_message(message):
+    data = message.to_dict() # turning the message into data to send as a dict
+    json_text = json.dumps(data) + "\n" # serializaing the data as json (adduing a newline)
 
-    pass
+    # using .encode() turns the python sting into bytes (essentially adding the b'{...}\n' from slides)
+    # then we send said bytes to the connected client, from myself (the server)
+    client_connection.sendall(json_text.encode())
+
+    print(f"Sent: uuid={message.uuid}, flag={message.flag}")
 
 def run_server():
     pass
